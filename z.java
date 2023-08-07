fa799e07f46e4e78ecc04c0e4dc35ce675fefeff
WangJiangZhou <1634846162@qq.com>
2023-08-07 16:45:12 +0800

    wjz

---
+++ z.java
@@ -0,0 +1,11 @@
+/**
+ * @name: z
+ * @author: JZWang
+ * @date: 2023/8/7 15:42
+ * @description:
+ */
+public class z {
+    public static void main(String[] args) {
+        System.out.println("a");
+    }
+}
