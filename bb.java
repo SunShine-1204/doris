2edaaeaad2363b64e54127957954fc7d04b4f37b
WangJiangZhou <1634846162@qq.com>
2023-08-07 16:47:42 +0800

    wjz

---
+++ bb.java
@@ -0,0 +1,11 @@
+/**
+ * @name: bb
+ * @author: JZWang
+ * @date: 2023/8/7 16:47
+ * @description:
+ */
+public class bb {
+    public static void main(String[] args) {
+        System.out.println("s");
+    }
+}
